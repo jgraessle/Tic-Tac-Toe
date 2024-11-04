de999585355716d196d952dc875aabb8d8901bdc
jgraessle <j_graessle@mail.harpercollege.edu>
2024-11-03 21:46:36 -0800

    New gameplay feature: Player vs. Computer
    A new feature was added that allows the player to play against a computer that moves into a random, empty box on the tic-tac-toe board each turn. A new game mode decision menu and exit menu were also added.

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,20 +2,91 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.Random;
 
 public class TicTacToe {
     Scanner in = new Scanner(System.in);
+    int menuChoice = 0;
+    int exitChoice = 0;
     int choice = 0;
     int player = 1;
     int winCondition = 0;
     ArrayList<String> movesList = new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9"));
     ArrayList<Boolean> repeatList = new ArrayList<>(Arrays.asList(false, false, false, false, false, false, false, false, false));
-    
+
+    public void ExitMenu() {
+        System.out.println("Would you like to continue playing or exit the game?");
+        System.out.println("1: Continue Playing");
+        System.out.println("2: Exit the game");
+        while (true)
+        try {
+            while (true) {
+            exitChoice = in.nextInt();
+                if (menuChoice == 1 || menuChoice == 2) {
+                    switch (exitChoice) {
+                        case 1:
+                            for (int i = 0; i < movesList.size(); i++) {
+                                movesList.set(i, "" + (i + 1) + "");
+                            }
+                            for (int i = 0; i < movesList.size(); i++) {
+                                repeatList.set(i, false);
+                            }
+                            menuChoice = 0;
+                            exitChoice = 0;
+                            choice = 0;
+                            player = 1;
+                            winCondition = 0;
+                            MenuDecision();
+                            Output();
+                            TicTacToeRunner();
+                            break;
+                        case 2:
+                            System.out.println("Exiting the game...");
+                            System.exit(0);
+                            break;
+                    }
+                }
+                System.out.print("Please input an integer of 1 or 2: ");
+            }
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Please input an integer 1 or 2");
+            in.nextLine();
+        } 
+    }
+    public int MenuDecision() {
+        System.out.println("Choose game type:");
+        System.out.println("1: Player vs Player");
+        System.out.println("2: Player vs Computer");
+        while (true) {
+            try {
+                menuChoice = in.nextInt();
+                if (menuChoice == 1 || menuChoice == 2) {
+                    return menuChoice;
+                }
+                else {
+                    System.out.print("Please input a choice of 1 or 2: ");
+                }
+            }
+            catch (InputMismatchException e){
+                System.out.println("Please input an integer");
+                in.nextLine();
+            }
+        }
+    }
     public void TicTacToeRunner() {
         while (winCondition == 0) {
-            Decision();
-            WinConditions();
-            Output();
+            if (player == 1 && menuChoice == 2) {
+                Decision();
+                WinConditions();
+                System.out.println("Your move:");
+                Output();
+            }
+            else {
+                Decision();
+                WinConditions();
+                Output();
+            }
             if (player == 1) {
                 player = player + 1;
             }
@@ -26,27 +97,57 @@ public class TicTacToe {
         switch (winCondition) {
             case 1: System.out.println("Player 1 wins!");
             break;
-            case 2: System.out.println("Player 2 wins!");
+            case 2: 
+            if (menuChoice == 1) {
+                System.out.println("Player 2 wins!");
+            }
+            else {
+                System.out.println("Computer wins!");
+            }
             break;
             case 3: System.out.println("There is a draw, how else did you expect tic tac toe to end?");
             break;
             default: System.out.println("Error with winCondition switch loop");
         }
+        ExitMenu();
     }
     public void Decision() {
         int choice = -1;
         while (choice < 1 || choice > 9 || repeatList.get(choice - 1) == true) {
-            if (choice < 1 || choice > 9) {
-                System.out.println("Player "+ player + ", please choose a move that is an integer between 1 and 9: ");
+            if (menuChoice == 2 && player == 2) {
+
+            }
+            else if (choice < 1 || choice > 9 ) {
+                System.out.print("Player "+ player + ", please choose a move that is an integer between 1 and 9: ");
             }
             else if (repeatList.get(choice -1) == true) {
-                System.out.println("Player " + player + ", please choose a move that hasn't already been taken: ");
+                System.out.print("Player " + player + ", please choose a move that hasn't already been taken: ");
             }
-            try { 
-                choice = in.nextInt();
-            } catch (InputMismatchException e) {
-                in.nextLine();
-                choice = -1;
+            if (menuChoice == 2) {
+                if (player == 1) {
+                    try { 
+                        choice = in.nextInt();
+                    } catch (InputMismatchException e) {
+                        in.nextLine();
+                        choice = -1;
+                    }
+                }
+                else if (player == 2) {
+                        Random rand = new Random();
+                        choice = rand.nextInt(9) + 1;
+                        System.out.println("Computer's move below:");
+                        while(repeatList.get(choice - 1) == true) {
+                            choice = rand.nextInt(9) + 1;
+                        }
+                }
+            }
+            else {
+                try { 
+                    choice = in.nextInt();
+                } catch (InputMismatchException e) {
+                    in.nextLine();
+                    choice = -1;
+                }
             }
         }
         if (player == 1) {
@@ -125,7 +226,10 @@ public class TicTacToe {
         }
     }
     public int WinConditions() {
-        if (movesList.get(0) == movesList.get(1) && movesList.get(2) == movesList.get(3)) {
+        if (repeatList.get(0) == true && repeatList.get(1) == true && repeatList.get(2) == true && repeatList.get(3) == true && repeatList.get(4) == true && repeatList.get(5) == true && repeatList.get(6) == true && repeatList.get(7) == true && repeatList.get(8) == true) {
+            winCondition = 3;
+        }
+        if (movesList.get(0) == movesList.get(1) && movesList.get(1) == movesList.get(2)) {
             PlayerCondition();
         }
         if (movesList.get(3) == movesList.get(4) && movesList.get(4) == movesList.get(5)) {
@@ -149,9 +253,6 @@ public class TicTacToe {
         if (movesList.get(2) == movesList.get(4) && movesList.get(4) == movesList.get(6)) {
             PlayerCondition();
         } 
-        if (repeatList.get(0) == true && repeatList.get(1) == true && repeatList.get(2) == true && repeatList.get(3) == true && repeatList.get(4) == true && repeatList.get(5) == true && repeatList.get(6) == true && repeatList.get(7) == true && repeatList.get(8) == true) {
-            winCondition = 3;
-        }
         return winCondition;
     }
     public void Output() {
@@ -163,7 +264,8 @@ public class TicTacToe {
     }
 public static void main(String[] args){
     TicTacToe tictactoe = new TicTacToe();
+    tictactoe.MenuDecision();
     tictactoe.Output();
     tictactoe.TicTacToeRunner();
-    }
+}
 }
